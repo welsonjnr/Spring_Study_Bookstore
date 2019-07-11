95988e85238b5c48cf078d3b2fec99e17c0479ca
Welson Júnior <welsongt9@live.com>
2019-07-10 22:18:14 -0300

    Project Created

---
+++ src/main/java/com/estudospring/livraria/LivrariaEstudoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.estudospring.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaEstudoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaEstudoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudospring/livraria/LivrariaEstudoSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.estudospring.livraria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LivrariaEstudoSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
