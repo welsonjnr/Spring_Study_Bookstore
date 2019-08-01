abaea6eef82215acbca0e3edcd5344488111eea8
Welson Júnior <welsongt9@live.com>
2019-08-01 15:27:00 -0300

    Creation of delete and finAll methods of class categoryService

--- src/main/java/com/estudospring/livraria/services/CategoryService.java
+++ src/main/java/com/estudospring/livraria/services/CategoryService.java
@@ -1,8 +1,10 @@
 package com.estudospring.livraria.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.estudospring.livraria.domain.Category;
@@ -11,38 +13,59 @@ import com.estudospring.livraria.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repo;
-	
+
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id);
 
-		if(obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-					+ ", Tipo: " + Category.class.getName());
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Category.class.getName());
 		}
-		
+
 		return obj.orElse(null);
-		}	
-	
-	public Category insert (Category obj) {
-		//Para garantir que não colocar no lugar de outra categoria que tenha o mesmo id
+	}
+
+	public Category insert(Category obj) {
+		// Para garantir que não colocar no lugar de outra categoria que tenha o mesmo
+		// id
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Category update(Category obj) {
-		//Vou criar um novo obj que vai encrontar o meu objeto que quero editar pelo id
+		// Vou criar um novo obj que vai encrontar o meu objeto que quero editar pelo id
 		Category newObj = find(obj.getId());
-		//Método a ser implementado no Resource
+		// Método a ser implementado no Resource
 		updateData(newObj, obj);
-		//Vou retornar o novo objeto salvo e editado
+		// Vou retornar o novo objeto salvo e editado
 		return repo.save(newObj);
 	}
 
+	public void delete(Integer idObj) {
+		// Encontra o objeto com o id passado como parâmetro
+		find(idObj);
+		try {
+			repo.deleteById(idObj);
+		}
+		// O try/catch é necessáro pois se ouver alguma outra classe relacionada com
+		// essa classe atual é necessário outro tratamento
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException(
+					"Não foi possível excluir, poís, possuem outras categorias relacionada a essa!");
+		}
+
+	}
+
+	//O comando repo.findAll() foi utilizado da própria IDE. Não sendo necessária implementa-lá
+	public List<Category> findAll(){
+		return repo.findAll();
+	}
 	
-	//Tu vai fazer esse método para que pegue as informações da categoria que são possiveis de serem mudadas e mudar e retornar mata o método update
+	// Tu vai fazer esse método para que pegue as informações da categoria que são
+	// para serem mudadas e mudar e retornar para o método update
 	private void updateData(Category newObj, Category obj) {
 		newObj.setName(obj.getName());
 	}
