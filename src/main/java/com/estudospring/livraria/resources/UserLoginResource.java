77593ae4b23a46a8ad6a577fc8f582e30494053d
Welson Júnior <welsongt9@live.com>
2021-03-19 08:54:52 -0300

    Implementação do delete

--- src/main/java/com/estudospring/livraria/resources/UserLoginResource.java
+++ src/main/java/com/estudospring/livraria/resources/UserLoginResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,11 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.estudospring.livraria.domain.Book;
-import com.estudospring.livraria.domain.Loan;
 import com.estudospring.livraria.domain.UserLogin;
-import com.estudospring.livraria.dto.BookDTO;
-import com.estudospring.livraria.dto.LoanDTO;
 import com.estudospring.livraria.dto.UserLoginDTO;
 import com.estudospring.livraria.services.UserLoginService;
 import com.estudospring.livraria.services.exceptions.ErrorAuthentication;
@@ -74,6 +71,13 @@ public class UserLoginResource {
 		user  = service.update(user);
 	
 		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value="/{id}")
+	@Transactional
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.ok().build();
 	}
 	
 }
